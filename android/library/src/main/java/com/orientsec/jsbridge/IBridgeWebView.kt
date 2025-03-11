72995219b9d48f185217fc1af25a622f51164298
meihu <meihu@orientsec.com.cn>
2025-03-11 17:22:46 +0800

    修改asset文件夹目录名称； 优化callHandler默认参数

--- android/library/src/main/java/com/orientsec/jsbridge/IBridgeWebView.kt
+++ android/library/src/main/java/com/orientsec/jsbridge/IBridgeWebView.kt
@@ -126,7 +126,7 @@ fun IBridgeWebView.install(channel: MessageChannel): JsBridge {
 fun IBridgeWebView.loadJsBridgeScript() {
     val mill = measureTimeMillis {
         try {
-            val js = view.context.assets.open("jsbridge/index.min.js")
+            val js = view.context.assets.open("js_bridge/index.min.js")
                 .bufferedReader()
                 .use {
                     val sb = StringBuilder()
